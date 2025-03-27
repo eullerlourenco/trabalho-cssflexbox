5ccb05cac7e286f3eda5d23bfab6db61db205639
els2003 <eullerlou321@gmail.com>
2025-03-27 20:13:42 -0300

    final version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-function changeTheme() {
+const btnChangeTheme = document.getElementById('btnChangeTheme');
+
+btnChangeTheme.addEventListener('click', function(event) {
     document.body.classList.toggle('dark-mode');
-}
+
+    if(document.body.classList.contains('dark-mode')) {
+        event.target.classList.remove('bi-moon');
+        event.target.classList.add('bi-brightness-high');
+    } else {
+        event.target.classList.remove('bi-brightness-high');
+        event.target.classList.add('bi-moon');
+    }
+});
